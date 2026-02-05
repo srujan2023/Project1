74c9fffc26dc76d4d759a39e8374dd014d790df6
Srujan <Srujan2004ab@gmail.com>
2026-02-05 12:30:43 +0530

    Database is Created

--- students/models.py
+++ students/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    usn=models.CharField(max_length=11,unique=True)
+    email=models.EmailField(unique=True)
+    phone=models.BigIntegerField(max_length=10,unique=True)
+    college=models.CharField(max_length=100)
+    degree=models.CharField(max_length=100)
+    branch=models.CharField(max_length=100)
+    sem=models.IntegerField()
+    
+    def __str__(self):
+     return self.name
+    
--- students/views.py
+++ students/views.py
@@ -7,4 +7,25 @@ def index(request):
 
 
 def register(request):
+    if request.method=='POST':
+        name=request.POST.get('sname')
+        usn=request.POST.get('usn')
+        email=request.POST.get('email')
+        phone=request.POST.get('mob')
+        college=request.POST.get('college')
+        degree=request.POST.get('degree')
+        branch=request.POST.get('branch')
+        sem=request.POST.get('sem')
+        
+        
+        print("Student Name:",name)
+        print("USN",usn)
+        print("Email",email)
+        print("Phone",phone)
+        print("degree",degree)
+        print("college",college)
+        print("Branch",branch)
+        print("sem",sem)
+        
+        
     return render(request,'register.html')
